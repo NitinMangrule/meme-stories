d5454ca6461dd602fc522362527c22ec238747fe
Nitin Mangrule <ndmangrule@gmail.com>
2025-04-15 19:52:18 +0530

    Added header

--- src/components/StoriesList/StoriesList.tsx
+++ src/components/StoriesList/StoriesList.tsx
@@ -9,7 +9,13 @@ interface StoriesListProps {
 
 const StoriesList: React.FC<StoriesListProps> = ({ stories, onSelect }) => {
   return (
-    <div className="fixed top-0 left-0 w-full bg-black z-50 px-2 py-4">
+    <div className="fixed top-0 left-0 w-full bg-black z-50  py-4">
+      <h1
+        className="w-full text-center text-2xl font-bold mb-5 mt-[-15px] text-white tracking-wide border-b-4 bg-gradient-to-tr from-yellow-400 to-pink-500 p-4 shadow-lg"
+        style={{ fontFamily: "'Billabong', cursive" }}
+      >
+        Meme Stories
+      </h1>
       <div className="flex overflow-x-auto no-scrollbar">
         {stories.map((story, idx) => (
           <StoryItem
